97d07c7842bec04a4694c441bb21f8f0ee77107e
japhex <j@japhex.net>
2019-10-03 11:15:43 +0100

    Moving logic to core

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TabStyled, TabContentStyled, TabsContainer, TabsContentContainer} from './styling'
 
-export const Tabs = ({children, onClick, activeTab}) => (
-	<div>
-		<TabsContainer>
-			{children.map(tab => (
-				<TabStyled onClick={() => onClick(tab.props.label)} activeTab={activeTab} tabLabel={tab.props.label}>{tab.props.label}</TabStyled>
-			))}
-		</TabsContainer>
-		<TabsContentContainer>
-			{children.map(tab => (
-				activeTab === tab.props.label && (<TabContentStyled>{tab.props.children}</TabContentStyled>)
-			))}
-		</TabsContentContainer>
-	</div>
-)
+export const Tabs = ({children}) => {
+	const [activeTab, setActiveTab] = useState('Tab 1')
+
+	const onClick = (tab) => {
+		setActiveTab(tab)
+	}
+
+	return (
+		<div>
+			<TabsContainer>
+				{children.map(tab => (
+					<TabStyled onClick={() => onClick(tab.props.label)} activeTab={activeTab} tabLabel={tab.props.label}>{tab.props.label}</TabStyled>
+				))}
+			</TabsContainer>
+			<TabsContentContainer>
+				{children.map(tab => (
+					activeTab === tab.props.label && (<TabContentStyled>{tab.props.children}</TabContentStyled>)
+				))}
+			</TabsContentContainer>
+		</div>
+	)
+}
 
 export const Tab = (props) => {
 	const { children } = props
--- stories/4-Tabs.stories.js
+++ stories/4-Tabs.stories.js
@@ -5,24 +5,16 @@ export default {
   title: 'Tabs',
 }
 
-export const Base = () => {
-	const [activeTab, setActiveTab] = useState('Tab 1')
-
-	const onClick = (tab) => {
-		setActiveTab(tab)
-	}
-
-	return (
-		<Tabs onClick={onClick} activeTab={activeTab}>
-			<Tab label="Tab 1">
-				Tab 1 content
-			</Tab>
-			<Tab label="Tab 2">
-				Tab 2 content
-			</Tab>
-			<Tab label="Tab 3">
-				Tab 3 content
-			</Tab>
-		</Tabs>
-	)
-}
+export const Base = () => (
+	<Tabs>
+		<Tab label="Tab 1">
+			Tab 1 content
+		</Tab>
+		<Tab label="Tab 2">
+			Tab 2 content
+		</Tab>
+		<Tab label="Tab 3">
+			Tab 3 content
+		</Tab>
+	</Tabs>
+)
