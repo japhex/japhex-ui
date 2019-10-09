1be3f1ec55f5c084acce82ff41535426e7f9e7b9
japhex <j@japhex.net>
2019-10-09 10:13:47 +0100

    Icons

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ export {Modal} from './components/modal'
 export {Tabs, Tab} from './components/tabs'
 export {Card, CardContent, CardFooter} from './components/card'
 export {MoreText} from './components/more-text'
+
+// Icons
+export * from './utils/icons'
